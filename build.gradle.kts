beb6b80d360a7e86ae9a9dbab1a15f81abb12c11
jaba <jabannishe@gmail.com>
2025-01-06 02:48:34 +0300

    add all transactions operations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.4.0"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("org.springframework.boot") version "3.0.6"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "1.8.10"
+    kotlin("plugin.allopen") version "2.1.0"
+//    kotlin("plugin.lombok") version "2.1.0"
+//    id("io.freefair.lombok") version "8.10"
 }
 
-group = "lab.is"
+group = "lab.is.bank"
 version = "0.0.1-SNAPSHOT"
+description = "Course project for information systems"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -19,23 +21,21 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-graphql")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.springframework:spring-webflux")
-    testImplementation("org.springframework.graphql:spring-graphql-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
-    }
+    testImplementation("org.springframework.security:spring-security-test")
+    runtimeOnly("org.postgresql:postgresql")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    // annotations("com.another.Annotation", "com.third.Annotation")
 }
